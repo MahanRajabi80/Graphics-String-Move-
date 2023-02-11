1d5c47e294cc1074d122c6454886b7d66c56f723
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 19:30:39 +0330

    Add files via upload

---
+++ Example 17-2/Example 17-2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Example_17_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowAnimate = new System.Windows.Forms.Button();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnShowAnimate
+            // 
+            this.btnShowAnimate.Location = new System.Drawing.Point(170, 60);
+            this.btnShowAnimate.Name = "btnShowAnimate";
+            this.btnShowAnimate.Size = new System.Drawing.Size(94, 39);
+            this.btnShowAnimate.TabIndex = 1;
+            this.btnShowAnimate.Text = "Show Animation";
+            this.btnShowAnimate.UseVisualStyleBackColor = true;
+            this.btnShowAnimate.Click += new System.EventHandler(this.btnShowAnimate_Click);
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(135, 22);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(168, 20);
+            this.txtString.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter String:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 445);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.btnShowAnimate);
+            this.Name = "Form1";
+            this.Text = "Example 17-2 (Graphics String Move)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowAnimate;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Example 17-2/Example 17-2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_17_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //------------------------------------------------------------------------------------
+        private void btnShowAnimate_Click(object sender, EventArgs e)
+        {
+            if (txtString.Text == "")
+            {
+                MessageBox.Show("TextBox is empty!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtString.Focus();
+                return;
+            }
+            string moveText = txtString.Text;
+            int x=10,stop_y=200;
+            Graphics gr = this.CreateGraphics();
+            gr.Clear(this.BackColor);
+            Font stringFont = new Font("Arial", 50);
+            for (int index = 0; index < moveText.Length; index++)//all chars
+            {
+                int width1Char = (int)gr.MeasureString(moveText[index].ToString(), stringFont).Width;
+                int height1Char = (int)gr.MeasureString(moveText[index].ToString(), stringFont).Height;
+                for (int y = this.Height; y >= stop_y; y--)
+                {
+                    gr.DrawString(moveText[index].ToString(), stringFont, Brushes.Black, x, y);
+                    for (int k = 0; k <= 1000000; k++) ;//delay
+                    if (y != stop_y)
+                    {
+                        gr.DrawString(moveText[index].ToString(), stringFont, SystemBrushes.Control, x, y);
+                    }
+                }
+                x += width1Char;
+            }
+        }
+    }
+}
